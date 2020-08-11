5ee351c067be76ae08773e6119b80432e8567410
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2020-08-12 01:15:23 +0900

    Change number of grid columns

--- lib/screens/GridListScreen.dart
+++ lib/screens/GridListScreen.dart
@@ -21,9 +21,9 @@ class GridListScreen extends StatelessWidget {
         body: GridView.count(
           // Create a grid with 2 columns. If you change the scrollDirection to
           // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
+          crossAxisCount: 3,
           // Generate 100 widgets that display their index in the List.
-          children: List.generate(100, (index) {
+          children: List.generate(12, (index) {
             return Center(
               child: Text(
                 'Item $index',
